5f77c888719f2e5be3cb01df27e3e227914062d3
patriciolencina <patriciogabriellencinahello@gmail.com>
2019-10-15 11:14:32 +0700

    refactor: convert class component to functional

--- src/screens/TabNavigator/TabBar/index.js
+++ src/screens/TabNavigator/TabBar/index.js
@@ -1,59 +1,54 @@
-import * as React from 'react';
+import React, {useRef} from 'react';
 import {SafeAreaView, View, Animated, Easing, Dimensions} from 'react-native';
-import TabBarIcon from './TabBarIcon';
+import {path} from 'ramda';
+import TabBarIcon from '../TabBarIcon';
 import styles from './styles';
 const screenWidth = Math.round(Dimensions.get('window').width);
 const backgroundViewWidth = screenWidth / 2;
-export default class TabBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.animatedValue = new Animated.Value(0);
-  }
-  handleAnimation = () => {
-    Animated.timing(this.animatedValue, {
-      toValue: this.props.navigation.state.index * backgroundViewWidth,
-      duration: 200,
-      easing: Easing.ease,
-    }).start();
-  };
-  navigationHandler = routeName => {
-    this.props.navigation.navigate(routeName);
-  };
-  render() {
-    const {navigation} = this.props;
-    const routes = navigation.state.routes;
 
-    this.handleAnimation();
-    return (
-      <SafeAreaView style={styles.containerSafe}>
-        <View style={styles.container}>
-          <Animated.View
-            style={[
-              styles.backgroundView,
-              {width: backgroundViewWidth},
-              {
-                transform: [
-                  {
-                    translateX: this.animatedValue,
-                  },
-                ],
-              },
-            ]}
-          />
+const TabBar = ({navigation = {}}) => {
+  const animatedValue = useRef(new Animated.Value(0)).current;
 
-          {routes.map((route, index) => {
-            return (
-              <TabBarIcon
-                key={route.key}
-                routeName={route.routeName}
-                onPress={currentRoute => this.navigationHandler(currentRoute)}
-                focused={navigation.state.index === index}
-                index={index}
-              />
-            );
-          })}
-        </View>
-      </SafeAreaView>
-    );
-  }
-}
+  const routes = path(['state', 'routes'], navigation);
+  const navigationIndex = path(['state', 'index'], navigation);
+
+  Animated.timing(animatedValue, {
+    toValue: navigationIndex * backgroundViewWidth,
+    duration: 200,
+    easing: Easing.ease,
+  }).start();
+
+  return (
+    <SafeAreaView style={styles.containerSafe}>
+      <View style={styles.container}>
+        <Animated.View
+          style={[
+            styles.backgroundView,
+            {width: backgroundViewWidth},
+            {
+              transform: [
+                {
+                  translateX: animatedValue,
+                },
+              ],
+            },
+          ]}
+        />
+
+        {routes.map((route, index) => {
+          return (
+            <TabBarIcon
+              key={route.key}
+              routeName={route.routeName}
+              onPress={currentRoute => navigation.navigate(currentRoute)}
+              focused={navigationIndex === index}
+              index={index}
+            />
+          );
+        })}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default TabBar;
---
+++ src/screens/TabNavigator/TabBarIcon.js
@@ -0,0 +1,82 @@
+import React, {useEffect, useRef} from 'react';
+import {TouchableWithoutFeedback, View, Animated, Easing} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import styles from './TabBar/styles';
+
+const TabBarIcon = ({focused, onPress, index, routeName}) => {
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  if (focused) {
+    Animated.timing(animatedValue, {
+      toValue: 1,
+      duration: 200,
+      easing: Easing.ease,
+    }).start();
+  } else {
+    Animated.timing(animatedValue, {
+      toValue: 0,
+      duration: 50,
+      easing: Easing.ease,
+    }).start();
+  }
+
+  let icon = '';
+
+  switch (index) {
+    case 0:
+      icon = 'ios-home';
+      break;
+
+    case 1:
+      icon = 'ios-heart';
+      break;
+  }
+  const translateXObject = {
+    translateX: animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, index === 0 ? 2 : -2],
+    }),
+  };
+  return (
+    <TouchableWithoutFeedback onPress={() => onPress(routeName)}>
+      <View style={[styles.wrapIcon]}>
+        <Animated.View
+          style={[
+            styles.icon,
+            {
+              transform: [
+                {
+                  translateY: animatedValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [0, -15],
+                  }),
+                },
+                translateXObject,
+              ],
+            },
+          ]}>
+          <Icon name={icon} color="black" size={34} />
+        </Animated.View>
+        <Animated.Text
+          style={[
+            styles.textStyle,
+            {
+              transform: [
+                {
+                  translateY: animatedValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [0, -40],
+                  }),
+                },
+                translateXObject,
+              ],
+            },
+          ]}>
+          {routeName}
+        </Animated.Text>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default TabBarIcon;
